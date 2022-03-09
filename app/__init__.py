c2ab88a9f6c92b1bfc7615e4586e09f3dc34e9f7
Miles Inada <milesinada219@gmail.com>
2022-03-08 22:34:22 -1000

    added products endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -20,3 +20,12 @@ def greet_user(fname, lname):
 @app.route("/square/<int:num>")
 def square_num(num):
     return "<h1>%s square is: %s </h1>" % (num, num*num)
+
+    
+@app.route("/products")
+def products():
+    product_list = ["Apples", "Oranges", "Bananas"]
+    bullet_list = "".join(
+        "<li>%s</li>" % product for product in product_list
+    )
+    return "<ul>%s</ul>" % bullet_list
